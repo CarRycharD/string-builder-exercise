18c38fbec4784b3bc22a41610fd575230921ebb8
CarRycharD <v.richard999@gmail.com>
2021-09-27 17:59:40 +0200

    Create StringBuilderExcercise.java

---
+++ StringBuilderExcercise.java
@@ -0,0 +1,18 @@
+
+public class StringBuilderExcercise {
+
+	public static void main(String[] args) {
+		String sequence = "";
+		
+		for(int i = 0; i <10; i++) {
+			if (i % 2 == 0) {
+				sequence = sequence + i;
+			} else {
+				sequence = i + sequence;
+			}
+		}
+		
+		System.out.println(sequence);
+	}
+
+}
